86a15719574acd88bdd497f06182790fb00dd63d
Tommy Troy Lin <tommytroylin@gmail.com>
2015-12-15 12:48:16 +0800

    Fix regexes in webpack config

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js(x)?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] } },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015', 'react'] } },
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
