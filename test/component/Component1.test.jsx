1c73835a00cde14674499e10b632bf122944f6d0
Tommy Troy Lin <tommytroylin@gmail.com>
2017-03-20 14:24:12 +0800

    feat(test): make test pass

--- test/component/Component1.test.jsx
+++ test/component/Component1.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import assert from 'power-assert';
-import { Component1 } from '../../src/app/component/Component1';
+import Component1 from '../../src/app/component/Component1';
 
 describe('Tests for Component component', () => {
   it('Render without error', () => {
